d07d5099336c2a175ab528f3688becae0d6034d3
Thierry Jet (tjp4y) <thierry.jet@programmingforyou.com>
2021-04-23 13:31:15 +0200

    Create app from custom jumpstart template

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,20 @@ Rails.application.routes.draw do
 authenticate :user, lambda { |u| u.admin? } do
   mount Sidekiq::Web => '/sidekiq'
   namespace :madmin do
+    resources :notifications
+    resources :services
+    namespace :active_storage do
+      resources :attachments
+    end
+    namespace :active_storage do
+      resources :variant_records
+    end
+    namespace :active_storage do
+      resources :blobs
+    end
+    resources :users
+    resources :announcements
+    root to: "dashboard#show"
   end
 end
 
